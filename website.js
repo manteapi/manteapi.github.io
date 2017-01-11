c41ba4aaf5c7e6f97aaff56b2557da005f85cf6a
Pierre-Luc Manteaux <pierre-luc.manteaux@inria.fr>
2017-01-11 12:00:34 +0100

    Update website
    Former-commit-id: e50fd0561d40b0790588975edd6614417da5d348

--- website.js
+++ website.js
@@ -1,26 +1,26 @@
 //Object website
 var website = 
-{ 
-    //Method header - Generate a header for a webpage of the website
-    header : function(pathToMainWebDir)
-    {
-        header_string = '';
-        header_string += '<nav class"navbar navbar-inverse">';
-        header_string += '<div class="container-fluid">';
-        header_string += '<ul class="nav navbar-nav">';
-        header_string += '<li><a href="'+pathToMainWebDir+'/index.html">Home</a></li>';
-        header_string += '<li class="dropdown">';
-        header_string += '<a class="dropdown-toggle" data-toggle="dropdown" href="#">Thesis<span class="caret"></span></a>';
-        header_string += '<ul class="dropdown-menu">';
-        header_string += '<li><a href="'+pathToMainWebDir+'/thesis.html#thesis-research">Research</a></li>';
-        header_string += '<li><a href="'+pathToMainWebDir+'/thesis.html#thesis-publications">Publications</a></li>';
-        header_string += '<li><a href="'+pathToMainWebDir+'/thesis.html#thesis-teaching">Teaching</a></li>';
-        header_string += '</ul>';
-        header_string += '</li>';
-        header_string += '<li><a href="'+pathToMainWebDir+'/about.html">About</a></li>';
-        header_string += '</ul>';
-        header_string += '</div>';
-        header_string += '</nav>';
-        return header_string;
-    }
-};
+    { 
+        //Method header - Generate a header for a webpage of the website
+        header : function(pathToMainWebDir)
+        {
+            header_string = '';
+            header_string += '<nav class"navbar navbar-inverse">';
+            header_string += '<div class="container-fluid">';
+            header_string += '<ul class="nav navbar-nav">';
+            header_string += '<li><a href="'+pathToMainWebDir+'/index.html">Home</a></li>';
+            header_string += '<li class="dropdown">';
+            header_string += '<a class="dropdown-toggle" data-toggle="dropdown" href="#">Thesis<span class="caret"></span></a>';
+            header_string += '<ul class="dropdown-menu">';
+            header_string += '<li><a href="'+pathToMainWebDir+'/thesis.html#thesis-research">Research</a></li>';
+            header_string += '<li><a href="'+pathToMainWebDir+'/thesis.html#thesis-publications">Publications</a></li>';
+            header_string += '<li><a href="'+pathToMainWebDir+'/thesis.html#thesis-teaching">Teaching</a></li>';
+            header_string += '</ul>';
+            header_string += '</li>';
+            header_string += '<li><a href="'+pathToMainWebDir+'projects.html">Projects</a></li>';
+            header_string += '</ul>';
+            header_string += '</div>';
+            header_string += '</nav>';
+            return header_string;
+        }
+    };
